f7a97f66bcbff176d76b246f0c90bdf92f611ecb
chaimae <73223980+Cherbatski@users.noreply.github.com>
2024-08-24 21:20:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,6 @@ with st.sidebar:
   islands=st.selectbox('islands',{'Torgersen','Biscoe','Dream'})
   gender=st.selectbox('gender',{'female','male'})
   bill_length_mm=st.slider('bill length (mm)',32.1,59.6,43.9)
+  bill_depth_mm=st.slider('bill depth (mm)',13.1,21.5,17.2)
+  flipper_length_mm=st.slider('flipper length (mm)',172.0,231.0,201.0)
+  body_mass_g=st.slider('body mass g',2700.0,6300.0,4207.0)
