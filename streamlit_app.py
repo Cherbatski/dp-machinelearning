6bf5a6fb0decad4ab6584d493aa25a0a0742bdfc
chaimae <73223980+Cherbatski@users.noreply.github.com>
2024-08-24 20:59:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ with st.expander('Data'):
   st.write("**Raw Data**")
   my_data=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   my_data
-# bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g
+
 with st.expander('Data visualization'):
-  st.scatter_chart(data=my_data,x='bill_length_mm',y='body_mass_g')
+  st.scatter_chart(data=my_data,x='bill_length_mm',y='body_mass_g',color='speices')
